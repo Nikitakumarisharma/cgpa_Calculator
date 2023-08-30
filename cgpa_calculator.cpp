a13103996a89a2cca851c131d70f3aacd4ce6721
Nikitakumarisharma <113896209+Nikitakumarisharma@users.noreply.github.com>
2023-08-30 14:27:57 +0530

    Add files via upload
    A Calculator to calculate overall 'CGPA.'.

---
+++ cgpa_calculator.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int numYears;
+    cout << "Enter the number of years: ";
+    cin >> numYears;
+
+    vector<double> sgpa(numYears);
+
+    for (int year = 0; year < numYears; ++year) {
+        cout << "Enter SGPA for year " << year + 1 << ": ";
+        cin >> sgpa[year];
+    }
+
+    double totalSGPA = 0.0;
+    for (int year = 0; year < numYears; ++year) {
+        totalSGPA += sgpa[year];
+    }
+
+    double cgpa = totalSGPA / numYears;
+
+    cout << "Your overall CGPA is: " << cgpa << endl;
+
+    return 0;
+}
